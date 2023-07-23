00f617fdd4c0b736c0435d667a7d07290d8bb55b
fullnamemillie <fullnamemillie@gmail.com>
2023-07-23 14:54:45 +0300

    test

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  publicPath: `/vue-app-test/`,
   plugins: [vue()],
   resolve: {
     alias: {
